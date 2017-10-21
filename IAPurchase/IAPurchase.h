a1c380473321ca446bd5ea75f9a7e92bffb2d0c0
Towhid <m.towhid.islam@gmail.com>
2017-10-21 20:47:53 +0600

    Initial Commit

---
+++ IAPurchase/IAPurchase.h
@@ -0,0 +1,19 @@
+//
+//  IAPurchase.h
+//  IAPurchase
+//
+//  Created by Towhid Islam on 10/21/17.
+//  Copyright © 2017 Towhid Islam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IAPurchase.
+FOUNDATION_EXPORT double IAPurchaseVersionNumber;
+
+//! Project version string for IAPurchase.
+FOUNDATION_EXPORT const unsigned char IAPurchaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IAPurchase/PublicHeader.h>
+
+
